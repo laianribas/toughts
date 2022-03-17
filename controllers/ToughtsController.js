449d43dd12947b08632fb81906bba4772e8b272f
laianribas <laian67a@gmail.com>
2022-03-17 20:54:00 -0300

    criação de pensamento

--- controllers/ToughtsController.js
+++ controllers/ToughtsController.js
@@ -9,4 +9,27 @@ export default class ToughtsController {
     static async dashboard(req, res) {
         res.render('toughts/dashboard')
     }
+
+    static createTought(req, res) {
+        res.render('toughts/create')
+    }
+
+    static async createToughtPost(req, res) {
+        const tought = {
+            title: req.body.title,
+            UserId: req.session.userid
+        }
+
+        try {
+            await Tought.create(tought)
+
+            req.flash('message', 'Pensamento criado com sucesso!')
+
+            req.session.save(() => {
+                res.redirect('/toughts/dashboard')
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
 }
--- routes/toughtsRoutes.js
+++ routes/toughtsRoutes.js
@@ -3,6 +3,8 @@ const toughtsRoutes = Router()
 import ToughtsController from '../controllers/ToughtsController.js'
 import authMiddleware from '../middlewares/authMiddleware.js'
 
+toughtsRoutes.get('/add', authMiddleware, ToughtsController.createTought)
+toughtsRoutes.post('/add', authMiddleware, ToughtsController.createToughtPost)
 toughtsRoutes.get('/dashboard', authMiddleware, ToughtsController.dashboard)
 toughtsRoutes.get('/', ToughtsController.showToughts)
 
